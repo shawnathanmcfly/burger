a29c3947938b5f4392f2fe2c8b1554f7e5cd3a91
shawnathanmcfly <sachimasi9187@gmail.com>
2019-02-11 18:44:03 -0500

    cleardb added

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
 
-    host: 'localhost',
-    user: 'root',
-    password: 'alu1ndra1',
-    database: 'burger_db'
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'bc5912efc29b1a',
+    password: '92b28cf4',
+    database: 'heroku_868fcc3e40a880e'
 });
 
 module.exports = con;
